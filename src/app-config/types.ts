4e7382e350481c6059975d292c7e303ac616adb6
Benjamin Laird <86321957+ben-laird@users.noreply.github.com>
2025-04-01 13:48:48 -0500

    feat(feature/app-config): add app config
    add the ability to configure medical id

---
+++ src/app-config/types.ts
@@ -0,0 +1,30 @@
+export interface Condition {
+	name: string;
+	highPriority?: boolean;
+}
+
+export interface MedicalCondition extends Condition {
+	notes?: string;
+}
+
+export interface Medication extends Condition {
+	dose: string;
+	frequency: string;
+}
+
+export interface Allergy extends Condition {
+	kind: AllergyType;
+	effects?: string;
+}
+
+export type AllergyType = "food" | "medication";
+
+export interface MedicalEvent extends Condition {
+	administeredOn: Date;
+}
+
+export interface MedicalProvider {
+	name: string;
+	specialty: string;
+	phone?: string;
+}
