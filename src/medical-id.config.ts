06cbb3b8b664b5eba9d1c8e118e28a754407fdf6
Alsa Balsa <119389769+AlsaBalsa@users.noreply.github.com>
2025-04-01 23:44:04 -0400

    feat(feature/app-config): Add personal info
    Add name, pronouns, birthday, and meds.

--- src/medical-id.config.ts
+++ src/medical-id.config.ts
@@ -1,3 +1,8 @@
 import { defineConfig } from "./app-config/mod.ts";
 
-export default defineConfig(() => ({}));
+export default defineConfig(() => ({
+	name: "Allistar Grace Barrett",
+	pronouns: "they/them/theirs",
+	birthday: new Date(2003, 6, 29),
+	meds: [{ name: "fluoxetine", dose: "40mg", frequency: "1/day" }],
+}));
